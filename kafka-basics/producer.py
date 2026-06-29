7795ab7a78387d81ae0c0a263787df27ec7ed495
IdiotCoffee <ishaanmavinkurve16@gmail.com>
2026-06-29 08:24:50 +0530

    learnt kafka partitioners and how to add configurations to kafka in confluent_kafka, also playing around with CALLBACKS and batch-sizes

--- kafka-basics/producer.py
+++ kafka-basics/producer.py
@@ -1,10 +1,16 @@
 from confluent_kafka import Producer
 
+config = {
+    "bootstrap.servers": "localhost:9092",
+    "batch.size": 400,
+    "partitioner": "random",
+}
+
 # create a producer - connect it to my local Kafka
-p = Producer({"bootstrap.servers": "localhost:9092"})
+p = Producer(config)
 
 
-# producer function - this produces data into Kafka
+# this is known as a CALLBACK function - it is run upon completion of the produce() function.
 def producerKafka(err, msg):
     if err is not None:
         print("message delivery failed: ", err)
@@ -15,13 +21,13 @@ def producerKafka(err, msg):
 # sample data:
 data = [1, 2, 3, 4, 5, 6, 7, 8, 9, "hello", "from", "python"]
 
+for j in range(10):
+    for i in range(30):
+        # trigger any available producerKafka callbacks, from previous produce() runs - for backlog
+        p.poll(0)
 
-for d in data:
-    # trigger any available producerKafka callbacks, from previous produce() runs - for backlog
-    p.poll(0)
-
-    # connect to a topic, convert the data into utf-8 string, use producerKafka as a callback and produce to the kafka topic.
-    p.produce("first_topic_ishaan", str(d).encode("utf-8"), callback=producerKafka)
+        # connect to a topic, convert the data into utf-8 string, use producerKafka as a callback and produce to the kafka topic.
+        p.produce("first_topic_ishaan", str(i).encode("utf-8"), callback=producerKafka)
 
 # wait for outstanding messages to be delivered, and callbacks to producerKafka to be triggered.
 p.flush()
